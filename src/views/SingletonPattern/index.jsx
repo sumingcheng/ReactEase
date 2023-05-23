3edab7f89f4e8003229e3ec998953dd692c090bd
范晓豪 <fanxiaohaohaha@qq.com>
2023-05-23 09:48:44 +0800

    v6 路由配置

---
+++ src/views/SingletonPattern/index.jsx
@@ -0,0 +1,7 @@
+export function SingletonPattern() {
+  return (
+    <div>
+      <h1>Singleton Pattern</h1>
+    </div>
+  );
+}
---
+++ src/views/StrategyPattern/index.jsx
@@ -0,0 +1,7 @@
+export function StrategyPattern() {
+  return (
+    <div>
+      <h1>StrategyPattern</h1>
+    </div>
+  );
+}
