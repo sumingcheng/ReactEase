79437d89a68d2cfff14566efb7c7818847885ff1
范晓豪 <fanxiaohaohaha@qq.com>
2023-07-24 12:22:13 +0800

    fix：修改

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Routers from '@/router/index.jsx'
 import '@/assets/index.css'
+import {ConfigProvider} from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Routers />
-  </React.StrictMode>
+    <ConfigProvider locale={zhCN} theme={{
+      // token: {
+      //   "colorPrimaryBgHover": "#93aba4",
+      //   "colorPrimaryBorderHover": "#adb8b4",
+      //   "colorPrimary": "#1a7868",
+      //   "colorPrimaryBg": "#d2deda"
+      // },
+    }}>
+      <React.StrictMode>
+        <Routers/>
+      </React.StrictMode>
+    </ConfigProvider>
 )
