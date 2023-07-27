f6fd8848272e167365a1183fe0a0b40b04862973
范晓豪 <fanxiaohaohaha@qq.com>
2023-07-27 10:33:52 +0800

    fix：ESLint

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,20 @@ import '@/assets/index.css'
 import {ConfigProvider} from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<ConfigProvider locale={zhCN} theme={{
-    token: {
-    // "colorPrimaryBgHover": "#93aba4",
-    // "colorPrimaryBorderHover": "#adb8b4",
-    // "colorPrimary": "#1a7868",
-    // "colorPrimaryBg": "#d2deda"
-    },
-}}>
-    <React.StrictMode>
-        <Routers/>
-    </React.StrictMode>
-</ConfigProvider>)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <ConfigProvider
+        locale={zhCN}
+        theme={{
+            token: {
+            // "colorPrimaryBgHover": "#93aba4",
+            // "colorPrimaryBorderHover": "#adb8b4",
+            // "colorPrimary": "#1a7868",
+            // "colorPrimaryBg": "#d2deda"
+            },
+        }}
+    >
+        <React.StrictMode>
+            <Routers/>
+        </React.StrictMode>
+    </ConfigProvider>
+)
--- src/views/AdapterPattern/index.jsx
+++ src/views/AdapterPattern/index.jsx
@@ -1,7 +1,9 @@
-export function AdapterPattern () {
-  return (
-      <div>
-        <h1 className="text-orange-700 text-2xl">AdapterPattern</h1>
-      </div>
-  )
+import React from 'react'
+
+export function AdapterPattern() {
+    return (
+        <div>
+            <h1 className="text-orange-700 text-2xl">AdapterPattern</h1>
+        </div>
+    )
 }
